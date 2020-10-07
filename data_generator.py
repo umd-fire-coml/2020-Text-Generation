384f77e9ecb4c2361962971d7d994fd3c045b0f1
umcptasa <umcptasa@gmail.com>
2020-10-07 12:17:30 -0400

    Adding init and skeleton structure

--- data_generator.py
+++ data_generator.py
@@ -2,3 +2,18 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 import csv
+
+def __init__(self, doc_ids, batch_size=32, shuffle=True):
+    'Initialization'
+    self.batch_size = batch_size
+    self.doc_ids = doc_ids
+    self.shuffle = shuffle
+    self.generate_doc_ids()
+    self.on_epoch_end()
+
+def generate_doc_ids(self):
+    
+
+def on_epoch_end(self):
+    if self.shuffle == True:
+        np.random.shuffle(self.doc_ids)
