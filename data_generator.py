df91f96e11992d8a129e1fdf1666cc475659d305
umcptasa <umcptasa@gmail.com>
2020-10-12 16:25:00 -0400

    Finished implementation for data_generator (Implemented "generate_batch")

--- data_generator.py
+++ data_generator.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 import csv
+import xml.etree.ElementTree as ET
+from nysk.xml
 
 def __init__(self, doc_ids, batch_size=32, shuffle=True):
     'Initialization'
@@ -11,8 +13,22 @@ def __init__(self, doc_ids, batch_size=32, shuffle=True):
     self.generate_doc_ids()
     self.on_epoch_end()
 
-def generate_doc_ids(self):
-    
+def generate_batch(self):
+    'Creating a ElementTree with the xml file'
+    data_tree = ET.parse('nysk.xml')
+    data_root = data_tree.getroot()
+
+    'Opens file for writing'
+    word_set = open("WordDataset.txt", "w")
+
+    'Finds all document tags and their corresponding text contents'
+    for description in myroot.findall('document'):
+        doc_text = description.find('text').text
+        'Splits the description string content into individual words in a list'
+        split_words = doc_text.split()
+        for words in split_words:
+            word_set.write(words + "\n")
+    word_set.close()
 
 def on_epoch_end(self):
     if self.shuffle == True:
